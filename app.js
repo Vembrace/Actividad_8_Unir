c4544cba9c32e9a401a7feb7b733fc2dc3fcf366
Vembrace <67844104+Vembrace@users.noreply.github.com>
2023-11-13 21:33:36 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const inmueblesRoutes = require('./api/routes/inmuebles');
+
+app.use(bodyParser.json());
+
+app.use('/api', inmueblesRoutes);
+
+// Manejo de errores
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Algo salió mal en el server !(');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor en ejecución en el puerto ${PORT}`);
+});
