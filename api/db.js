2173ae47723daf086aa9a7218b43d873452c1eb5
Vembrace <67844104+Vembrace@users.noreply.github.com>
2023-11-13 21:28:42 +0000

    Delete api/db.js

--- api/db.js
+++
@@ -1,7 +0,0 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
-
-//mongoose.connect('mongodb://0.0.0.0:27017/inmuebleDB?directConnection=true');
-const uri = "mongodb://0.0.0.0:27017";
-const mongo = new MongoClient(uri);
-
-module.exports = mongo;
