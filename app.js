8a141f1aeb9846ea85fe705638251a34005df1f2
edinsonuwu <Vembrace@gmail.com>
2023-12-01 10:12:13 -0500

    t2

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use('/api', inmueblesRoutes);
 // Manejo de errores
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Algo salió mal en el server !.(');
+  res.status(500).send('Algo salió mal en el server !(');
 });
 
 const PORT = process.env.PORT || 3000;
